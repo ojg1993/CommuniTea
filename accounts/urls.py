b00d6d37c6ce2fce837dd47eee717911c8f781e5
Jingyu <ojg1993@gmail.com>
2023-12-15 16:52:37 +0900

    feat: implement user logout
    - Created "user_logout" view using auth.logout().
    - Modified "user_login" URL pattern for consistency.
    - Updated templates to include the appropriate URL mappings for both user login and logout.

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("register", views.register, name="register"),
-    path("user_login", views.user_login, name="user_login"),
-    path("logout", views.logout, name="logout"),
+    path("user-login", views.user_login, name="user-login"),
+    path("user-logout", views.user_logout, name="user-logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,38 +5,39 @@ from . import forms
 from django.contrib.auth.models import auth
 from django.contrib.auth import authenticate
 
+
 def register(request):
     form = forms.RegisterUserForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = forms.RegisterUserForm(request.POST)
         if form.is_valid():
             user = form.save()
             user.is_active = False
             user.save()
-            return redirect('home')
+            return redirect("home")
 
-    context = {'form':form}
+    context = {"form": form}
     return render(request, "accounts/registration/register.html", context=context)
+
 
 def user_login(request):
     form = forms.LoginForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = forms.LoginForm(request.POST)
         if form.is_valid():
-            email = request.POST.get('email')
-            password = request.POST.get('password')
-            print(email,":", password)
+            email = request.POST.get("email")
+            password = request.POST.get("password")
             user = authenticate(request, email=email, password=password)
-            print("user:", user)
 
             if user is not None:
                 auth.login(request, user)
-                print("login success")
-                return redirect('home')
+                return redirect("home")
             else:
                 print("login failed")
-    context = {'form':form}
+    context = {"form": form}
     return render(request, "accounts/login.html", context=context)
 
-def logout(request):
-    pass
+
+def user_logout(request):
+    auth.logout(request)
+    return redirect("home")
