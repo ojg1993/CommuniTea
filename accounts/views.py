4d0f24103467e5565011aa538778be0cd242e66d
Jingyu <ojg1993@gmail.com>
2023-12-19 18:13:30 +0900

    refactor: update user verification link based on server protocol
    - Update register view to generate email verification link using the protocol detected by the server.
    - Modify the template to use the generated link.

--- accounts/views.py
+++ accounts/views.py
@@ -23,13 +23,18 @@ def register(request):
             user = form.save()
 
             current_site = get_current_site(request)
+            link = f"{request.scheme}://{current_site.domain}"
+
             subject = "Account verification email for CommuniTea"
-            message = render_to_string('accounts/registration/email-verification.html', {
-                'user': user,
-                'domain': current_site.domain,
-                "uid": urlsafe_base64_encode(force_bytes(user.pk)),
-                "token": account_activation_token.make_token(user),
-            })
+            message = render_to_string(
+                "accounts/registration/email-verification.html",
+                {
+                    "user": user,
+                    "link": link,
+                    "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                    "token": account_activation_token.make_token(user),
+                },
+            )
             user.email_user(subject=subject, message=message)
             return redirect("email-verification-sent")
 
